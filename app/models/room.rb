15a61c31c222d516c66985e5298320543472f756
Mark Lehman & Harland Hauger <student@epicodus.com>
2014-04-17 14:32:55 -0700

    add highly rated rooms to front page, sort searches by rating

--- app/models/room.rb
+++ app/models/room.rb
@@ -11,4 +11,14 @@ class Room < ActiveRecord::Base
       find(:all)
     end
   end
+
+  def average_rating
+    ratings = []
+    if self.ratings.count > 0
+      self.ratings.each {|r| ratings << r.stars}
+      ratings.inject{ |sum, rating| sum + rating }.to_f / ratings.size
+    else
+      ratings = 0.0
+    end
+  end
 end
