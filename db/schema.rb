0201160529c3ab1dc2d39fc9195cf4a08dcad8a8
Mark Lehman <markopolo@gmail.com>
2014-06-01 14:38:45 -0700

    tweak front page design to show only featured pictures

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140601210454) do
+ActiveRecord::Schema.define(version: 20140601211049) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 20140601210454) do
     t.string   "caption"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "featured"
+    t.boolean  "featured",           default: false
   end
 
   create_table "ratings", force: true do |t|
